9ab93db15ae94622c3983c81fca217c4110415f5
Usov Nikita <nikitosu27@gmail.com>
2025-06-10 02:53:12 +0700

    just testing

--- SERVICE_MATH_RECOGNIZE/src/services/math_recognizer.py
+++ SERVICE_MATH_RECOGNIZE/src/services/math_recognizer.py
@@ -1,6 +1,9 @@
 import colorama
 from loguru import logger
 
+from PIL import Image
+from pix2tex import cli
+
 from src.core.config import ConfigLoader
 
 
@@ -9,8 +12,18 @@ from src.core.config import ConfigLoader
 class MathRecognizer:
     def __init__(self):
         self.config = ConfigLoader()
+        self.model = cli.LatexOCR()
+
+    
+    def image_to_latex(self, image_path: str) -> str:
+        return self.model(image_path)
      
 
     @logger.catch
     def RecognizeExpression(self, request):
-        return 23
+        img = Image.open('recognize_test/test2.jpg')
+        recognition_result = self.image_to_latex(img)
+        logger.info(f"recognition result: {colorama.Fore.YELLOW}{recognition_result}")
+        return recognition_result
+    
+    
