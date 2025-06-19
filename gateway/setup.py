cc1331d6faa8872e49fb569b95930d3846bd5721
Usov Nikita <nikitosu27@gmail.com>
2025-06-20 01:36:17 +0700

    gateway now works

--- gateway/setup.py
+++ gateway/setup.py
@@ -1,10 +1,13 @@
 import asyncio
 import sys
+import os
 
 import colorama
 from loguru import logger
 
-sys.path.insert(0, "./gen")  # Fix of relative import in generated stubs
+GEN_PATH = os.path.join(os.path.dirname(__file__), 'gen')
+sys.path.insert(0, GEN_PATH)  # Fix of relative import in generated stubs
+
 from src.core.config import ConfigLoader
 from src.core.logging import InterceptHandler, LogSetup
 from src.external_gateway.graphql_server import ExternalGatewayServer
