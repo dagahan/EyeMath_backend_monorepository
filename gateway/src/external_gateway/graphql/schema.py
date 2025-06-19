6dbf3b4a8ba107b83f1ea63cc510e7632ae21c92
Usov Nikita <nikitosu27@gmail.com>
2025-06-19 21:37:49 +0700

    recognize endpoint now live

--- gateway/src/external_gateway/graphql/schema.py
+++ gateway/src/external_gateway/graphql/schema.py
@@ -7,40 +7,64 @@ from src.grpc.client.factory_grpc_client import GRPCClientFactory
 
 
 @strawberry.type
-class MathSolution:
+class MathSolveGraphQLResponse:
     results: List[str]
     renders: List[str]
     solving_steps: List[str] | None = strawberry.field(description="Solving steps")
+
+
+@strawberry.type
+class MathRecognizeGraphQLResponse:
+    result: str
 
 
 class Shema:
     @strawberry.type
     class Query:
         @strawberry.field(description="Solve math expression in latex format.")
-        def solve_math(self,
+        def solve_math(
+        self,
         latex_expression: str = "2x = 10",
         show_solving_steps: bool = True,
         render_latex_expressions: bool = False,
-        ) -> MathSolution:
+        ) -> MathSolveGraphQLResponse:
             
             response = GRPCClientFactory.rpc_call(
                 service_name="math_solve",
                 method_name="solve",
                 latex_expression=latex_expression,
                 show_solving_steps=show_solving_steps,
-                render_latex_expressions=render_latex_expressions
+                render_latex_expressions=render_latex_expressions,
             )
             
-            # Преобразование gRPC ответа в GraphQL тип
-            return MathSolution(
+            return MathSolveGraphQLResponse(
                 results=response.results,
                 renders=response.renders,
-                solving_steps=response.solving_steps
+                solving_steps=response.solving_steps,
+            )
+
+
+        @strawberry.field(description="Recognize math expression in latex format from image.")
+        def recognize_latex(
+        self,
+        image: str,
+        normalize_for_sympy: bool = True,
+        ) -> MathRecognizeGraphQLResponse:
+            
+            response = GRPCClientFactory.rpc_call(
+                service_name="math_recognize",
+                method_name="recognize",
+                image=image,
+                normalize_for_sympy=normalize_for_sympy,
+            )
+            
+            return MathRecognizeGraphQLResponse(
+                result=response.result,
             )
         
 
     def create_graphql_router(self) -> GraphQLRouter:
         schema = strawberry.Schema(
-            query=self.Query
+            query=self.Query,
         )
         return GraphQLRouter(schema)
--- recognize_latex/src/services/math_latex_render.py
+++ recognize_latex/src/services/math_latex_render.py
@@ -22,17 +22,26 @@ class LatexRenderTool:
 
 
     async def save_image_localy(self, jpg_bytes: bytes) -> None:
+        '''
+        saves image localy in rendered images directory.
+        '''
         if self.save_render_img:
             directory = f"{self.rendered_img_dir}/image{FileSystemTools.count_files_in_dir(self.rendered_img_dir)}.jpg"
             with open(directory, "wb") as f:
                 f.write(jpg_bytes)
 
 
-    def convert_jpg_base64(self, jpg_bytes: bytes) -> str:
+    def convert_jpg_to_base64(self, jpg_bytes: bytes) -> str:
+        '''
+        converts jpg image from bytes to string of base64 format.
+        '''
         return base64.b64encode(jpg_bytes).decode('utf-8')
 
 
-    def render_latex_jpg(self, latex_expression: str, dpi: int) -> str:
+    def render_latex_jpg(self, latex_expression: str, dpi: int) -> bytes:
+        '''
+        renders an image in with jpg format and returns it bytes.
+        '''
         fig = plt.figure(figsize=(0.1, 0.1), dpi=dpi)
         plt.text(0.5, 0.5, f"${latex_expression}$",
                  fontsize=12,
@@ -49,11 +58,11 @@ class LatexRenderTool:
         plt.close(fig)
         
         buf.seek(0)
-        return str(buf.getvalue())
+        return buf.getvalue()
 
 
     def render_latex_jpg_base64(self, latex_expression: str, dpi: int) -> str:
         jpg_bytes = self.render_latex_jpg(latex_expression, dpi)
         asyncio.run(self.save_image_localy(jpg_bytes))
-        return self.convert_jpg_base64(jpg_bytes)
+        return self.convert_jpg_to_base64(jpg_bytes)
     
